cf3208d991e1209b60c559e00095d99436b00025
ll-sie <e.ndiramiye@alustudent.com>
2023-11-10 13:46:08 +0200

    added more functionalitiels to update, delete, view all, and see completed todos

--- my-todo/src/components/Todos.js
+++ my-todo/src/components/Todos.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux';
-import { addTodos, removeTodos } from '../redux/reducer';
+import { addTodos, removeTodos, updateTodos, completeTodos } from '../redux/reducer';
 
 const mapStateProps = (state) => {
   return  {
@@ -12,17 +12,22 @@ const mapDispatchToProps = (dispatch) => {
   return {
     addTodo: (obj) => dispatch(addTodos(obj)),
     removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (id) => dispatch(completeTodos(id)),
   }
 }
 
 const Todos = (props) => {
 
+    
+
     const [todo, setTodo] = useState("");
+
 
     const handleChange = (e) =>{
         setTodo(e.target.value)
     };
-    console.log("props from store", props);
+    // console.log("props from store", props);
   return (
     <div className='addTodos'>
       <input type="text" onChange={(e) => handleChange(e)} className='todo-input'/>
@@ -32,16 +37,6 @@ const Todos = (props) => {
         completed: false,
       })}>Add</button>
       <br/>
-      <ul>
-        {props.todos.map((item) => {
-          return (
-          <li key={item.id}>
-          <textarea>{item.item}</textarea>  
-          <button>Edit</button>
-          <button onClick={() => props.removeTodo(item.id)}>Delete</button>{" "}
-          </li>);
-        })}
-      </ul>
     </div>
   )
 }
